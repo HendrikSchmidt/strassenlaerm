1802b32a48374aa5505ff405f85d767bd9e7c253
Hendrik Schmidt <hendriknas@gmail.com>
2021-01-14 19:19:33 +0100

    Update mapbox.js

--- mapbox.js
+++ mapbox.js
@@ -8,48 +8,29 @@ const map = new mapboxgl.Map({
 
 const layers = ['strassen', 'plaetze'];
 
-map.on('load', function() {
-    // When a click event occurs on a feature in the places layer, open a popup at the
-    // location of the feature, with description HTML from its properties.
-    map.on('click', 'strassen', function(e) {
-        const coordinates = e.features[0].geometry.coordinates.slice()[0];
-        const name = e.features[0].properties.name;
-        const description = e.features[0].properties.description;
-        const html = '<b>' + name + '</b><br/>' + description + '<br/><a href="/objekt/#' + name + '"> > mehr </a>';
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(html)
-            .addTo(map);
-    });
-
-    map.on('click', 'plaetze', function(e) {
-        const coordinates = e.features[0].geometry.coordinates.slice()[0][4];
-        const name = e.features[0].properties.name;
-        const description = e.features[0].properties.description;
-        const html = '<b>' + name + '</b><br/>' + description + '<br/><a href="/objekt/#' + name + '"> > mehr </a>';
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(html)
-            .addTo(map);
-    });
-
+map.on('load', () => {
     layers.map(layer => {
+        // When a click event occurs on a feature in the places layer, open a popup at the
+        // location of the feature, with description HTML from its properties.
+        map.on('click', layer, (e) => {
+            let coordinates = e.features[0].geometry.coordinates[0];
+            coordinates = layer === 'plaetze' ? coordinates[0] : coordinates;
+            const name = e.features[0].properties.name;
+            const description = e.features[0].properties.description;
+            const html = '<b>' + name + '</b><br/>' + description + '<br/><a href="/objekt/#' + name + '"> > mehr </a>';
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(html)
+                .addTo(map);
+        });
         // Change the cursor to a pointer when the mouse is over the places layer.
         map.on('mouseenter', layer, () => {
             map.getCanvas().style.cursor = 'pointer';
