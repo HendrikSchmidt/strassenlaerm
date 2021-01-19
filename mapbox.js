80d021ee29a9042df7dce5760825e146094e1ea7
Hendrik Schmidt <hendriknas@gmail.com>
2021-01-19 23:29:49 +0100

    Update mapbox.js

--- mapbox.js
+++ mapbox.js
@@ -9,7 +9,6 @@ const map = new mapboxgl.Map({
 const layers = ['strassen', 'plaetze'];
 
 map.on('load', () => {
-    console.log('loaded')
     layers.map(layer => {
         // When a click event occurs on a feature in the places layer, open a popup at the
         // location of the feature, with description HTML from its properties.
@@ -34,7 +33,6 @@ map.on('load', () => {
         });
         // Change the cursor to a pointer when the mouse is over the places layer.
         map.on('mouseenter', layer, () => {
-            console.log(layer)
             map.getCanvas().style.cursor = 'pointer';
         });
         // Change it back to a pointer when it leaves.
