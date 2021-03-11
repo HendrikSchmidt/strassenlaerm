7b34e38d1caaa793d520007f55feb90b853569d3
Hendrik Schmidt <hendriknas@gmail.com>
2021-03-11 18:17:27 +0100

    rename folder, add plugin for mapbox upload

---
+++ wordpress/mapbox-transfer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Plugin Name: Mapbox Transfer
+*/
+
+add_filter('bulk_actions-edit-post', function($bulk_actions) {
+    $bulk_actions['transfer-to-mapbox'] = __('In Mapbox laden', 'txtdomain');
+    return $bulk_actions;
+});
+
+add_filter('handle_bulk_actions-edit-post', function($redirect_url, $action, $post_ids) {
+    $map_objects = [];
+    if ($action == 'transfer-to-mapbox') {
+        foreach ($post_ids as $post_id) {
+            $object_data = get_fields($post_id);
+            $object_data['name'] = get_the_title($post_id);
+            $map_objects[ $post_id ] = $object_data;
+        }
+        $command = escapeshellcmd(ABSPATH . '/wp-content/strassenlaerm/data-upload/upload_data_to_datasets.py' . json_encode($map_objects));
+        $output = shell_exec($command);
+        $redirect_url = add_query_arg('transfered-to-mapbox', $output, $redirect_url);
+    }
+    return $redirect_url;
+}, 10, 3);
+
+add_action('admin_notices', function() {
+    if (!empty($_REQUEST['transfered-to-mapbox'])) {
+        $num_changed = (int) $_REQUEST['transfered-to-mapbox'];
+        printf('<div id="message" class="updated notice is-dismissible"><p>' . __('Transfered %d objects to Mapbox.', 'txtdomain') . '</p></div>', $num_changed);
+    }
+});
