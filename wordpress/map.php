c43b75c6735ab51874f670260b13fb67327f4d77
Hendrik Schmidt <hendriknas@gmail.com>
2021-03-17 03:24:56 +0100

    change link and script loading

--- wordpress/map.php
+++ wordpress/map.php
@@ -15,28 +15,26 @@ if ( $map_query -> have_posts() ) :
     while ( $map_query -> have_posts() ) : $map_query -> the_post();
         $object_data = get_fields();
         $object_data['name']= get_the_title();
-        $object_data['link']= get_permalink();
+        $object_data['link']= get_the_permalink();
         $map_objects[ get_the_id() ] = $object_data;
-        echo get_permalink();
     endwhile;
 endif;
 ?>
-
-<script>
-    const mapObjects = <?php echo json_encode($map_objects); ?>;
-</script>
-
 <div class="container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>">
     <div class="container">
         <main class="template-map content <?php avia_layout_class( 'content' ); ?> units" <?php avia_markup_helper(array('context' => 'content'));?>>
             <div class="entry-content-wrapper entry-content clearfix">
-                <link href="/wp-content/strassenlaerm/map/map.css" rel="stylesheet" />
                 <div id="map"><object-information></object-information></div>
-                <script type="text/javascript" src="/wp-content/strassenlaerm/map/objectInformation.js"></script>
-                <script type="text/javascript" src="/wp-content/strassenlaerm/map/mapbox.js"></script>
             </div><!--end content-->
         </main>
     </div><!--end container-->
 </div><!-- close default .container_wrap element -->
 <?php
+function mapbox_enqueue() {
+    wp_enqueue_script( 'object-information-js', '/wp-content/strassenlaerm/map/objectInformation.js' );
+    wp_enqueue_script( 'custom-mapbox-js', '/wp-content/strassenlaerm/map/mapbox.js' );
+    wp_add_inline_script( 'custom-mapbox-js', 'const mapObjects = <?php echo json_encode($map_objects); ?>;' );
+    wp_enqueue_style( 'map-css', '/wp-content/strassenlaerm/map/map.css' );
+}
+add_action( 'wp_enqueue_scripts', 'mapbox_enqueue' );
 get_footer();
