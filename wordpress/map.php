2eee51ff0a5195636aa81d5260db63055a723028
Hendrik Schmidt <hendriknas@gmail.com>
2021-03-17 03:57:14 +0100

    Update map.php

--- wordpress/map.php
+++ wordpress/map.php
@@ -30,17 +30,13 @@ endif;
     </div><!--end container-->
 </div><!-- close default .container_wrap element -->
 <?php
-//function mapbox_enqueue() {
 wp_enqueue_script( 'mapbox-gl.js', 'https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.js' );
 wp_enqueue_style( 'mapbox-gl.css', 'https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css' );
 wp_enqueue_script( 'bootstrap.bundle.min.js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js' );
 wp_enqueue_style( 'bootstrap.min.css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css' );
-wp_enqueue_style( 'map-css', 'wp-content/strassenlaerm/map/map.css', array(), false, true );
-wp_enqueue_script( 'object-information-js', 'wp-content/strassenlaerm/map/objectInformation.js', array(), false, true );
-wp_enqueue_script( 'custom-mapbox-js', 'wp-content/strassenlaerm/map/mapbox.js', array(), false, true );
-/*wp_add_inline_script( 'custom-mapbox-js', 'const mapObjects = <?php echo json_encode($map_objects); ?>;' );*/
-wp_add_inline_script( 'custom-mapbox-js', 'console.log("test");' );
-//}
-//add_action( 'wp_enqueue_scripts', 'mapbox_enqueue' );
+wp_enqueue_style( 'map-css', content_url() . '/strassenlaerm/map/map.css', array(), false, true );
+wp_enqueue_script( 'object-information-js', content_url() . '/strassenlaerm/map/objectInformation.js', array(), false, true );
+wp_enqueue_script( 'custom-mapbox-js', content_url() . '/strassenlaerm/map/mapbox.js', array(), false, true );
+wp_add_inline_script( 'custom-mapbox-js', 'const mapObjects = ' . json_encode($map_objects) );
 wp_footer();
 get_footer();
