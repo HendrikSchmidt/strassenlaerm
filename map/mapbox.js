96ea9e16141643ce751809e90b7564ea16191d38
Hendrik Schmidt <hendriknas@gmail.com>
2021-03-19 23:10:52 +0100

    Update mapbox.js

--- map/mapbox.js
+++ map/mapbox.js
@@ -93,7 +93,9 @@ function loadDescription() {
     expPopup.remove();
     if(location.hash) {
         const id = parseInt(location.hash.split('-')[0].substr(1));
-        const clickedObj = features.find(feature => feature.id === id);
+        console.log(id)
+        // const clickedObj = features.find(feature => feature.properties.wp_id === id);
+        const clickedObj = features[0];
         const props = mapObjects[id];
         document.querySelector('object-information').object = {
             link: props.link,
