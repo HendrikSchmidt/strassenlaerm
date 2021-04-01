3f6e51aab867bb49a1f584e4d3698600619b01f3
Hendrik Schmidt <hendriknas@gmail.com>
2021-04-01 21:10:58 +0200

    create recipe for all layers

--- data-upload/update_tilesets_sources.py
+++ data-upload/update_tilesets_sources.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import json
 import requests
@@ -5,27 +6,38 @@ from mapbox import Datasets
 from dotenv import load_dotenv
 load_dotenv()
 
+if len(sys.argv) != 3:
+    print('Please supply a mode <create|publish> and tile set <plaetze|strassen|viertel|denkmaeler|gebaeude>.')
+    sys.exit(1)
+
+mode = sys.argv[1]
+tileset = sys.argv[2]
 url = 'https://api.mapbox.com/tilesets/v1/'
-datasets = Datasets(access_token=os.getenv("dataset_token"))
-dataset_to_tileset = [
-    (os.getenv("squares_dataset"), 'plaetze'),
-    (os.getenv("streets_dataset"), 'strassen')
-]
+access_string = '?access_token=' + os.getenv("tileset_token")
 
-for dataset_id in dataset_to_tileset:
-    collection = datasets.list_features(dataset_id[0]).json()
+
+if 'source' in mode:
+    dataset_id = os.getenv(tileset + '_dataset')
+    datasets = Datasets(access_token=os.getenv("dataset_token"))
+    collection = datasets.list_features(dataset_id).json()
     features = collection['features']
-    ld_features = ''
-    for feature in features:
-        ld_features += json.dumps(feature) + '\n'
+    ld_features = '\n'.join(json.dumps(feature) for feature in features)
+    files = {'file': (tileset + '.geojson', ld_features)}
+    sources_url = url + 'sources/strassenlaerm/' + tileset + access_string
+    if mode == 'create_source':
+        r = requests.post(sources_url, files=files)
+    elif mode == 'replace_source':
+        r = requests.put(sources_url, files=files)
+    else:
+        print('Please use a valid mode.')
+        sys.exit(1)
+elif mode == 'create_tileset':
+    with open('recipe-' + tileset + '.json', 'r') as recipe_file:
+        r = requests.post(url + 'strassenlaerm.' + tileset + access_string, json=json.loads(recipe_file.read()))
+elif mode == 'publish_tileset':
+    r = requests.post(url + 'strassenlaerm.' + tileset + '/publish' + access_string)
+else:
+    print('Please use a valid mode.')
+    sys.exit(1)
 
-    files = {'file': (dataset_id[1] + '.geojson', ld_features)}
-
-    r = requests.put(
-        url + 'sources/strassenlaerm/' + dataset_id[1] + '?access_token=' + os.getenv("tileset_token"),
-        files=files
-    )
-    print(r.text)
-
-    r = requests.post(url + 'strassenlaerm.' + dataset_id[1] + '/publish?access_token=' + os.getenv("tileset_token"))
-    print(r.text)
+print(r.text)
