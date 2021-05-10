8032a947275561e1f14900c7dc4235a3e71c3245
Hendrik Schmidt <hendriknas@gmail.com>
2021-05-10 23:27:24 +0200

    use correct key for height

--- map/mapbox.js
+++ map/mapbox.js
@@ -5,7 +5,7 @@ let lastPosition = { center, zoom };
 const pad = 50;
 
 const header = document.getElementById('header');
-const headerHeight = header?.style.height ?? 0;
+const headerHeight = header?.offsetHeight ?? 0;
 document.getElementById('map').style.height = `${window.innerHeight - headerHeight}px`;
 
 const map = new mapboxgl.Map({
